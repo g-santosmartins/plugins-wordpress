485e03da85e6da4cda6da85fce8e921bb5eb3d21
Guilherme Martins <g.santosmartins@outlook.com>
2021-08-09 16:44:29 -0300

    Terminando o primeiro plugin para patrocinadores

---
+++ al_local_dia_palestra/al_local_dia_palestra.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * Plugin Name: Cadastrar local e data da palestra
+ * Description: Puglin para cadastrar local e data da prócima palestra realizada pela Alura
+ * Version: 1.0.0
+ * Author: Guilherme Martins
+ */
+
+if (!defined('ABSPATH')) {
+    die;
+}
+
+require_once plugin_dir_path(__FILE__) . '/includes/al_local_dia_palestra_settings.php';
---
+++ al_patrocinadores_palestras/al_patrocinadores_palestras.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * Plugin Name: Patrocinadores Alura
+ * Description: Selecionar patrocinadores da palestra da Alura
+ * Version: 1.0.0
+ * Author: Guilherme Martins
+ */
+
+if(!defined('ABSPATH')){
+    die;
+}
+
+require_once plugin_dir_path(__FILE__) . '/includes/al_patrocinadore_palestras_widget.php';
