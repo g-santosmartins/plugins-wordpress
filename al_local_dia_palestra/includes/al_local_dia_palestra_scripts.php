37f0c4e5e20deaafc16530f011bcd4c97c932b94
Guilherme Martins <g.santosmartins@outlook.com>
2021-08-11 13:26:16 -0300

    Terminando a tradução e puglin de temperatura

--- al_local_dia_palestra/includes/al_local_dia_palestra_scripts.php
+++ al_local_dia_palestra/includes/al_local_dia_palestra_scripts.php
@@ -1,16 +1,15 @@
 <?php
 
-add_action('wp_enqueue_scripts', 'al_local_dia_palestra_adiciona_scripts');
+add_action('wp_enqueue_scripts','al_local_dia_palestra_adiciona_scripts');
+function al_local_dia_palestra_adiciona_scripts(){
 
-    // Javascripts
-function al_local_dia_palestra_adiciona_scripts() {
+    //Javascript
     wp_enqueue_script(
         'jquery_countdown_lib',
         plugins_url() . '/al_local_dia_palestra/js/jquery.countdown.min.js',
         array('jquery'),
         null,
         true
-
     );
 
     wp_enqueue_script(
@@ -19,26 +18,19 @@ function al_local_dia_palestra_adiciona_scripts() {
         null,
         null,
         true
-
     );
 
-
     //Estilos - CSS
-
     wp_enqueue_style(
         'al_local_dia_palestra_estilos',
         plugins_url() . '/al_local_dia_palestra/css/al_local_dia_palestra_estilos.css'
     );
 
-
-    //Data cadastrada no menu de config
-
+    //Data cadastrado no menu de configurações
     $data = get_option('al_local_dia_palestra_data');
-
     wp_localize_script(
         'al_local_dia_palestra_contagem_regressiva',
         'data',
         $data
     );
-
 }
